2e6aa4a01cc6cb345031f2d4ac57eac4d71855eb
KosukeOmura <social.666.k@gmail.com>
2020-10-27 15:07:17 +0900

    DB接続

--- index.php
+++ index.php
@@ -17,11 +17,53 @@ define('PDO_DSN','mysql:host=localhost;dbname=' . DB_DATABASE);
 date_default_timezone_set('Asia/Tokyo');
 
 //変数の初期化
+$db = null;
+$now_date = null;
+$sql = null;
+$clean = array();
+$stmt = null;
 
+
+try { 
 
     // データベースに接続
-    $mysqli = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $db = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
+            // 書き込み日時を取得
+            $now_date = date("Y-m-d H:i:s");
+            
+            // データを登録するSQL作成
+            $sql = "INSERT INTO box (do1, do2, time) VALUES ( '$clean[do1]', '$clean[do2]', '$now_date')";
+            
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+
+            $db = null;
+}catch(Exception $e) {
+    echo 'ただいまメンテナンス中';
+    exit();
+}
+
+try { 
+
+    // データベースに接続
+    $db = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+            // 書き込み日時を取得
+            $now_date = date("Y-m-d H:i:s");
+            
+            // DBからデータ出力
+            $sql = 'SELECT id,do1,do2,time FROM box WHERE 1';
+            
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+
+    }catch(Exception $e) {
+        echo 'ただいまメンテナンス中';
+        exit();
+    }
 
 
  ?>
@@ -45,22 +87,24 @@ date_default_timezone_set('Asia/Tokyo');
 
     <form method="post">
         <div class="input-area">
-            <input type="text" class="one" name="do1" placeholder="選択①">
-            <input type="text" class="two" name="do2" placeholder="選択②">
-            <input type="submit" name="btn_submit" value="GO!">
+                <input type="text" class="do1" name="do1" placeholder="選択①">
+                <input type="text" class="do2" name="do2" placeholder="選択②">
+                <input type="submit" name="btn_submit" value="GO!">
         </div>
     </form>
 
     <section>
             <article>
                 <div class="info">
+                    
                 <?php
-                    $r = rand(0,1);
-                    if ($r == 0) {
-                        echo 'do1';
-                    }else {
-                        echo 'do2';
-                    }
+                            $r = rand(0,1);
+                            if ($r == 0) {
+                                echo 'do1';
+                            }else {
+                                echo 'do2';
+                            }
+
                 ?>
                 </div>
             </article>
