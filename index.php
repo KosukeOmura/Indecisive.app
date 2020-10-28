ec4c6d46a6717139a0a99f95b10a82228008415e
KosukeOmura <social.666.k@gmail.com>
2020-10-28 15:33:51 +0900

    ランダム選択完成

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-// エラー表示を有効にする
-ini_set( 'display_errors', 1 );
+// // エラー表示を有効にする
+// ini_set( 'display_errors', 1 );
 
-// 全てのエラーを表示する
-ini_set('error_reporting', E_ALL);
+// // 全てのエラーを表示する
+// ini_set('error_reporting', E_ALL);
 
 
 // DB接続情報
@@ -22,28 +22,52 @@ $now_date = null;
 $sql = null;
 $clean = array();
 $stmt = null;
+$error_message = array();
 
+if( !empty($_POST['btn_submit']) ) {
+    // do1の入力チェック
+    if( empty($_POST['do1'])) {
+        $error_message[] = '選択①を入力してください';
+    }else {
+        $clean['do1'] = htmlspecialchars( $_POST['do1'], ENT_QUOTES);
+    
+        //セッションに表示名を保存
+        $_SESSION['do1'] = $clean['do1'];
+    }
+    // do2の入力チェック
+    if( empty($_POST['do2'])) {
+        $error_message[] = '選択①を入力してください';
+    }else {
+        $clean['do2'] = htmlspecialchars( $_POST['do2'], ENT_QUOTES);
+    
+        //セッションに表示名を保存
+        $_SESSION['do2'] = $clean['do2'];
+    }
 
-try { 
+    try { 
 
-    // データベースに接続
-    $db = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
-    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-
-            // 書き込み日時を取得
-            $now_date = date("Y-m-d H:i:s");
-            
-            // データを登録するSQL作成
-            $sql = "INSERT INTO box (do1, do2, time) VALUES ( '$clean[do1]', '$clean[do2]', '$now_date')";
-            
-            $stmt = $db->prepare($sql);
-            $stmt->execute();
-
-            $db = null;
-}catch(Exception $e) {
-    echo 'ただいまメンテナンス中';
-    exit();
+        // データベースに接続
+        $db = new PDO(PDO_DSN,DB_USERNAME,DB_PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+                // 書き込み日時を取得
+                $now_date = date("Y-m-d H:i:s");
+                
+                // データを登録するSQL作成
+                $sql = "INSERT INTO box (do1, do2, time) VALUES ( '$clean[do1]', '$clean[do2]', '$now_date')";
+                
+                $stmt = $db->prepare($sql);
+                $stmt->execute();
+    
+                $db = null;
+    }catch(Exception $e) {
+        echo 'ただいまメンテナンス中';
+        exit();
+    }
 }
+
+
+
 
 try { 
 
@@ -60,10 +84,13 @@ try {
             $stmt = $db->prepare($sql);
             $stmt->execute();
 
+
     }catch(Exception $e) {
         echo 'ただいまメンテナンス中';
         exit();
     }
+
+
 
 
  ?>
@@ -87,8 +114,8 @@ try {
 
     <form method="post">
         <div class="input-area">
-                <input type="text" class="do1" name="do1" placeholder="選択①">
-                <input type="text" class="do2" name="do2" placeholder="選択②">
+                <input type="text" class="do1" name="do1" placeholder="選択①" value="<?php if( !empty($_SESSION['do1']) ){ echo $_SESSION['do1']; } ?>">
+                <input type="text" class="do2" name="do2" placeholder="選択②" value="<?php if( !empty($_SESSION['do2']) ){ echo $_SESSION['do2']; } ?>">
                 <input type="submit" name="btn_submit" value="GO!">
         </div>
     </form>
@@ -96,16 +123,21 @@ try {
     <section>
             <article>
                 <div class="info">
-                    
-                <?php
-                            $r = rand(0,1);
-                            if ($r == 0) {
-                                echo 'do1';
-                            }else {
-                                echo 'do2';
-                            }
 
+                <?php
+                    $do1 = $_POST['do1'];
+                    $do2 = $_POST['do2'];
                 ?>
+                
+                    <?php
+                                $r = rand(0,1);
+                                if ($r == 0) {
+                                    echo $do1;
+                                }else {
+                                    echo $do2;
+                                }
+
+                    ?>
                 </div>
             </article>
     </section>
